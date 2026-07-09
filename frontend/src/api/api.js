c21193a0210b1992ac51dda029589004d24c4d8d
JNSathyasri <jnsathyasri@gmail.com>
2026-07-10 03:03:17 +0530

    Update frontend API URL for EC2 deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
-  timeout: 10000,
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:5000/api",
+//   timeout: 10000,
+// });
+
+const API = axios.create({
+    baseURL: "http://54.226.83.25:5000/api"
 });
 
 api.interceptors.response.use(
