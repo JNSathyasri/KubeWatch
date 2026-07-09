a89a92ae97a8770437fe3c6f64a723454a8c509c
JNSathyasri <jnsathyasri@gmail.com>
2026-07-10 03:09:51 +0530

    Fix API endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,25 +1,21 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "http://127.0.0.1:5000/api",
-//   timeout: 10000,
-// });
-
-const API = axios.create({
-    baseURL: "http://54.226.83.25:5000/api"
+const api = axios.create({
+    baseURL: "http://54.226.83.25:5000/api",
+    timeout: 10000,
 });
 
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("API Error:", error);
+    (response) => response,
+    (error) => {
+        console.error("API Error:", error);
 
-    if (error.response) {
-      console.error("Status:", error.response.status);
+        if (error.response) {
+            console.error("Status:", error.response.status);
+        }
+
+        return Promise.reject(error);
     }
-
-    return Promise.reject(error);
-  }
 );
 
 export default api;
